5483503a940e1c87f6af7074be6ae68e1b84efd8
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2018-02-12 01:32:28 -0300

    relação dos currículos

--- crawler_lattes.py
+++ crawler_lattes.py
@@ -11,6 +11,8 @@ for curriculo in curriculos:
     id = curriculo['id10']
     name = curriculo['name'].split(' ')[0].lower()
 
+    # http://buscacv.cnpq.br/buscacv/rest/espelhocurriculo/7165875430419020 JSON
+    # http://buscacv.cnpq.br/buscacv/#/espelho?nro_id_cnpq_cp_s=7165875430419020 XML
     url = urllib2.urlopen('http://buscacv.cnpq.br/buscacv/rest/download/curriculo/%s' % id)
     zipFile = zipfile.ZipFile(StringIO.StringIO(url.read()))
     curriculoXML = zipFile.read('curriculo.xml')
